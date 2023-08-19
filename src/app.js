39501ce8c5c365ca9b3553b358422d9886819d40
HolidayGuy21 <14449@coderacademy.edu.au>
2023-08-19 16:29:37 +1000

    added video streaming functionality

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import fs from 'fs'
+
+const app = express()
+
+app.get('/video', (req, res) => {
+    const range = req.headers.range ?? "0-";
+    if (!range) {
+        res.status(400).send('Requires range header');
+    }
+    const videoPath = "Waterfall.mp4";
+    const videoSize = fs.statSync("Waterfall.mp4").size;
+
+    const CHUNK_SIZE = 10 ** 6; 
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, {start, end})
+
+    videoStream.pipe(res)
+});
+
+const port = 9000
+app.listen(port)
+

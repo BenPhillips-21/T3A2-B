112fe66125644d58a4dc9cd2380414eca61692f6
Zak Mohamed <zakeriyaamohamed@gmail.com>
2023-08-23 17:26:59 +1000

    do not call the devs 'goofy ahh'

--- src/database/conn.js
+++ src/database/conn.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 
 export default async function connect(){
    await mongoose.connect(process.env.ATLAS_DB_URI)
-    .then(goose => console.log(goose.connection.readyState === 1 ? 'Mongoose Connected' : 'Moongoose failed to connect yo goofy ahh'))
+    .then(goose => console.log(goose.connection.readyState === 1 ? 'Mongoose Connected' : 'Moongoose failed to connect.'))
     .catch(err => console.log(err))
 }
